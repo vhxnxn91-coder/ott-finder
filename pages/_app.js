35b3b01fbbbc06c270dd788ff906f2ec714f2b4b
vhxnxn91-coder <vhxnxn91@gmail.com>
2026-08-18 15:05:03 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {});
+    }
+  }, []);
   return <Component {...pageProps} />;
 }
